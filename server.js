8c679a2f31540801285cbffbc4b81611365b3241
wufeiafei <468008903@qq.com>
2018-05-20 22:47:57 +0800

    get/post request.

--- server.js
+++ server.js
@@ -8,8 +8,28 @@ var querystring = require('querystring');
 function startServer(route, handle) {
     var onRequest = function(request, response) {
         var pathname = url.parse(request.url).pathname;
-        console.log('Request received ' + request.url);
-        route(handle, request.url, response);
+        console.log('Request received ' + pathname);
+        var data = [];
+        request.on("error", function(err) {
+            console.error(err);
+
+        }).on("data", function(chunk) {
+            data.push(chunk);
+            console.log("chunk:",chunk);
+        }).on('end', function() {
+            if (request.method === "POST") {
+                if (data.length > 1e6) {
+                    request.connection.destroy();
+                }
+                data = Buffer.concat(data).toString();
+                var body = querystring.parse(data);  //将一个字符串反序列化为一个对象
+                console.log("body:",body);
+                route(handle, pathname, response, querystring.parse(data));
+            } else {
+                var params = url.parse(request.url, true).query;
+                route(handle, pathname, response, params);
+            }
+        });
     }
 
     var server = http.createServer(onRequest);
